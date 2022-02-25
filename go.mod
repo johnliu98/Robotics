26322b07ab327ee4a4a02498326de0165dd9f622
John Liu <johnliu@kth.se>
2022-02-25 21:55:25 +0100

    feat(sys): add getter methods

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/johnliu98/robotics
 
 go 1.17
 
-require gonum.org/v1/gonum v0.9.3
+require (
+	github.com/jinzhu/copier v0.3.5
+	gonum.org/v1/gonum v0.9.3
+)
--- sys/sys.go
+++ sys/sys.go
@@ -2,28 +2,64 @@ package sys
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/jinzhu/copier"
 	"gonum.org/v1/gonum/mat"
 )
 
 type System struct {
-	Nx    int
-	Nu    int
-	Time  float64
-	State *mat.VecDense
+	nx    int
+	nu    int
+	time  float64
+	state *mat.VecDense
 }
 
 func NewSystem(nx, nu int) *System {
 	return &System{
-		Nx:    nx,
-		Nu:    nu,
-		Time:  0.0,
-		State: mat.NewVecDense(nx, nil),
+		nx:    nx,
+		nu:    nu,
+		time:  0.0,
+		state: mat.NewVecDense(nx, nil),
 	}
+}
+
+func (s *System) Nx() int {
+	var nx int
+	err := copier.Copy(nx, s.nx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nx
+}
+
+func (s *System) Nu() int {
+	var nu int
+	err := copier.Copy(nu, s.nu)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nu
+}
+
+func (s *System) Time() float64 {
+	var t float64
+	err := copier.Copy(t, s.time)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return t
+}
+
+func (s *System) State() *mat.VecDense {
+	st := &mat.VecDense{}
+	st.CloneFromVec(s.state)
+	return st
 }
 
 func (s *System) String() string {
 	return fmt.Sprintf(
 		"Nx: %d\nNu: %d\nTime: %v\nState:\n%v",
-		s.Nx, s.Nu, s.Time, mat.Formatted(s.State),
+		s.nx, s.nu, s.time, mat.Formatted(s.state),
 	)
 }
