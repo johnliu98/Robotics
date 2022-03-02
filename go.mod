593244d16db945ae1d815ee1efb64b811f9db103
John Liu <johnliu@kth.se>
2022-03-02 17:24:11 +0100

    feat(main): plot pendulum simulation

--- go.mod
+++ go.mod
@@ -3,6 +3,20 @@ module github.com/johnliu98/robotics
 go 1.17
 
 require (
-	github.com/jinzhu/copier v0.3.5
 	gonum.org/v1/gonum v0.9.3
+	gonum.org/v1/plot v0.10.0
+)
+
+require (
+	github.com/ajstarks/svgo v0.0.0-20210923152817-c3b6e2f0c527 // indirect
+	github.com/badgerodon/lalg v0.0.0-20131221025232-d98d02968c5b // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/go-fonts/liberation v0.2.0 // indirect
+	github.com/go-latex/latex v0.0.0-20210823091927-c0d11ff05a81 // indirect
+	github.com/go-pdf/fpdf v0.5.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/exp v0.0.0-20210220032938-85be41e4509f // indirect
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.8 // indirect
 )
--- main.go
+++ main.go
@@ -2,18 +2,71 @@ package main
 
 import (
 	"fmt"
-	"github.com/johnliu98/robotics/sim/pen"
+    "image/color"
+	"time"
+
 	"github.com/johnliu98/robotics/sim"
-    "gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
+const T float64 = 10
+
 func main() {
-	p := pen.NewPendulum()
-	fmt.Println(p)
+	var s sim.Simulation = sim.NewPendulum()
 
-    var s sim.Simulation = pen.NewPendulum()
+	u := mat.NewVecDense(1, []float64{0.7})
+    dt := 0.02
+    N := int(T / dt)
 
-    u := mat.NewVecDense(1, []float64{1})
-    fmt.Println(s.Update(u, 0.02))
-    fmt.Println(s.Update(u, 0.02))
+    thData := make(plotter.XYs, N)
+    dthData := make(plotter.XYs, N)
+
+    // simulate pendulum
+	start := time.Now()
+	for i := 0; i < int(N); i++ {
+        v := s.Update(u, dt)
+		fmt.Println(i, v)
+
+        thData[i].X = s.Time()
+        thData[i].Y = v.AtVec(0)
+        dthData[i].X = s.Time()
+        dthData[i].Y = v.AtVec(1)
+	}
+
+    // print computation time
+	d := float64(time.Since(start)) / float64(N)
+    var unit string
+    switch i := d; {
+    case i > 1e9: unit = "s "; d /= 1e9
+    case i > 1e6: unit = "ms"; d /= 1e6
+    case i > 1e3: unit = "µs"; d /= 1e3
+    case i > 1e0: unit = "ns";
+    }
+	fmt.Printf("Computation time: %.2f %s", d, unit)
+
+    // plot data
+    p := plot.New()
+    p.Title.Text = "Pendulum"
+    p.X.Label.Text = "time [s]"
+    p.Add(plotter.NewGrid())
+
+    thLine, err := plotter.NewLine(thData)
+    if err != nil {
+        panic(err)
+    }
+    thLine.Color = color.RGBA{31, 119, 180, 255}
+	thLine.LineStyle.Width = vg.Points(2)
+
+    dthLine, err := plotter.NewLine(dthData)
+    if err != nil {
+        panic(err)
+    }
+    dthLine.Color = color.RGBA{255, 127, 14, 255}
+	dthLine.LineStyle.Width = vg.Points(2)
+
+    p.Add(thLine, dthLine)
+	p.Save(30*vg.Centimeter, 30*vg.Centimeter, "states.png")
 }
